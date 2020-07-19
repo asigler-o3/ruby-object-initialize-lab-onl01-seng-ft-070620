a68ad000d36aede536ce72a7fd4cd2dc1720b795
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-19 00:53:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,2 @@
 class Dog
-  def Dog (name)
-    @name = name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def Dog (breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
 end
